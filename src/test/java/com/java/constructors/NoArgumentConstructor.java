1fb78f885cd7400c2cbe7327415979bbec99cf34
mugundhan-1998 <mugusundar98@gmail.com>
2023-04-15 17:59:19 +0530

    static keyword

--- src/test/java/com/java/constructors/NoArgumentConstructor.java
+++ src/test/java/com/java/constructors/NoArgumentConstructor.java
@@ -5,29 +5,31 @@ public class NoArgumentConstructor {
 	String name;
 	int number;
 	Integer num;
-	
-	
-	 public NoArgumentConstructor(){
-		  name = "java"; 
-		  number = 5; 
-		  num = 2;  
-		  }
-	 
-	 public void NoArgumentConstructor(){
-		  name = "python"; 
-		  number = 51; 
-		  num = 21;  
-		  }
-	
-	public static void main(String[] args) {
-		NoArgumentConstructor con = new NoArgumentConstructor();
-		System.out.println("The value of variable name is "+ con.name);
-		System.out.println("The value of variable number is "+ con.number);
-		System.out.println("The value of variable num is "+ con.num);
-		con.NoArgumentConstructor();
-		System.out.println("The value of variable name is "+ con.name);
-		System.out.println("The value of variable number is "+ con.number);
-		System.out.println("The value of variable num is "+ con.num);
+
+	public NoArgumentConstructor() {
+		name = "java";
+		number = 5;
+		num = 2;
 	}
+
+	public void NoArgumentConstructor() {
+		name = "python";
+		number = 51;
+		num = 21;
+	}
+
+
+	public static void main(String[] args) {
+		NoArgumentConstructor con = new NoArgumentConstructor(); 
+		System.out.println("The value of variable name is "+ con.name);
+						 
+		System.out.println("The value of variable number is "+ con.number); 
+								
+		System.out.println("The value of variable num is "+ con.num);
+		
+	con.NoArgumentConstructor();
+				System.out.println("The value of variable name is "+ con.name);
+				System.out.println("The value of variable number is "+ con.number);
+				System.out.println("The value of variable num is "+ con.num); }
 
 }
